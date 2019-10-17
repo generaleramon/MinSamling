69a7142d26f58c06d0fad9b23b533af27c21a41c
Alberto <52979095+generaleramon@users.noreply.github.com>
2019-10-17 18:36:42 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sleep 10
 
-#Mount all partition with 10 seconds delay
+#Mount all fstab partition with 10 seconds delay
+sleep 10
 sudo mount -a
 
-#Undervolt(?) Rith amdctl
+#Undervolt(?) with amdctl
 sudo amdctl -v 88
 
-#Run (X)f.lux 
+#Run (X)f.lux (Upplands Väsby)
 xflux -l 59.52 -g 17.91 -k 4000
