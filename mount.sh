728bc44892589e08f5828afb4ab685d3d1a50bfa
Alberto <52979095+generaleramon@users.noreply.github.com>
2021-11-28 11:13:02 +0100

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 #sudo sysctl -p
-sudo mount -a
-#sudo mount 192.168.1.31:/media/raid5 /home/generaleramon/RAID5
-#sudo mount 192.168.1.31:/media/4GB /home/generaleramon/Documents
-#sudo mount 192.168.1.31:/media/4GB/Pictures /home/generaleramon/Pictures
-#sudo cpupower -c 4-5 frequency-set --governor ondemand
+#sudo mount 192.168.1.31:/xxx/xxx /xxx/xxx/xxx
 
